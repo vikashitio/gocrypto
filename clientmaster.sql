b7d5a3b014ea186862020269440ff5517937a0f6
Vikash Kumar Gupta <164337272+vikashitio@users.noreply.github.com>
2024-07-10 13:11:53 +0530

    Delete clientmaster.sql

--- clientmaster.sql
+++
@@ -1,3 +0,0 @@
-client_id,username,password,full_name,status,timestamp,volt_id
-149,vikashg@bigit.io,$2a$10$EIe6ddgGoNYMhhXXWm9Qhuv//XybMxs5G5XoIa7zaolwN56j/W0Ie,Vikash Gupta,1,2024-07-02 10:31:00.558736+05:30,
-72,vikashg@itio.in,$2a$10$LqnY1EE.6v2i7aUqG1xZH.YvIBCZL2LHiwUExo9Ut2kQN59THL9Xq,Vikash Kumar Gupta,1,2024-06-11 12:40:23.789831+05:30,1
